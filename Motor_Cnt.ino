f154d52471674843bf8ccc68f3b27f69f7ff693d
Diego <diego.gibellofoglio@gmail.com>
2018-03-19 11:56:09 +0100

    Up code and check the values of wr and wl

--- Motor_Cnt.ino
+++ Motor_Cnt.ino
@@ -5,7 +5,7 @@
 //#include <nav_msgs/Odometry.h>
 
 ros::NodeHandle  nh; // allows to create publisher/subscriber
-
+geometry_msgs::Twist vel;
 //nav_msgs::Odometry mov;
 
 #define EN 8
@@ -16,7 +16,7 @@ axis_t right, left;
 
 void motors_cb(const geometry_msgs::Twist &m_r){
   int wr,wl;
-  if (m_r.linear.x > 0 && m_r.angular.z == 0){
+  if (m_r.linear.x > 0 && m_r.angular.z == 0){ //va avanti
     wr=(m_r.linear.x/r);
     wl=(m_r.linear.x/r);
     StepperDriver.setDir (left, FORWARD);
@@ -24,7 +24,7 @@ void motors_cb(const geometry_msgs::Twist &m_r){
     StepperDriver.setSpeed (left, wr);
     StepperDriver.setSpeed (right, wl);
   }
-  else if (m_r.linear.x < 0 && m_r.angular.z == 0){
+  else if (m_r.linear.x < 0 && m_r.angular.z == 0){ // va indietro
     wr=(abs(m_r.linear.x)/r);
     wl=(abs(m_r.linear.x)/r);
     StepperDriver.setDir (left, BACKWARD);
@@ -32,25 +32,44 @@ void motors_cb(const geometry_msgs::Twist &m_r){
     StepperDriver.setSpeed (left, wr);
     StepperDriver.setSpeed (right, wl);
   }
-  else if (m_r.linear.x >= 0 && m_r.angular.z >= 0){
-    wr=(1/(2*r))*(m_r.angular.z*L-m_r.linear.x*2);
-    wl=(1/(2*r))*(m_r.angular.z*L+m_r.linear.x*2);
+  else if (m_r.linear.x == 0 && m_r.angular.z <= 0){  //gira a destra
+    wr=(1/(2*r))*(abs(m_r.angular.z)*L-m_r.linear.x*2);
+    wl=(1/(2*r))*(abs(m_r.angular.z)*L+m_r.linear.x*2);
     StepperDriver.setDir (left, FORWARD);
     StepperDriver.setDir (right, BACKWARD);
     StepperDriver.setSpeed (left, wr);
     StepperDriver.setSpeed (right, wl);
   }
-  else if (m_r.linear.x >= 0 && m_r.angular.z <= 0){
-    wr=(1/(2*r))*(abs(m_r.angular.z)*L+m_r.linear.x*2);
-    wl=(1/(2*r))*(abs(m_r.angular.z)*L-m_r.linear.x*2);
+  else if (m_r.linear.x == 0 && m_r.angular.z >= 0){  //gira a sinistra
+    wr=(1/(2*r))*(m_r.angular.z*L+m_r.linear.x*2);
+    wl=(1/(2*r))*(m_r.angular.z*L-m_r.linear.x*2);
     StepperDriver.setDir (left, BACKWARD);
     StepperDriver.setDir (right, FORWARD);
     StepperDriver.setSpeed (left, wr);
     StepperDriver.setSpeed (right, wl);
   }
+  else if (m_r.linear.x > 0 && m_r.angular.z <= 0){
+    wr=(1/(2*r))*abs((abs(m_r.angular.z)*L-m_r.linear.x*2));
+    wl=(1/(2*r))*abs((abs(m_r.angular.z)*L+m_r.linear.x*2));
+    StepperDriver.setDir (left, BACKWARD);
+    StepperDriver.setDir (right, BACKWARD);
+    StepperDriver.setSpeed (left, wr);
+    StepperDriver.setSpeed (right, wl);
+  }
+  else if (m_r.linear.x > 0 && m_r.angular.z >= 0){
+    wr=(1/(2*r))*abs((m_r.angular.z*L+m_r.linear.x*2));
+    wl=(1/(2*r))*abs((m_r.angular.z*L-m_r.linear.x*2));
+    StepperDriver.setDir (left, FORWARD);
+    StepperDriver.setDir (right, FORWARD);
+    StepperDriver.setSpeed (left, wr);
+    StepperDriver.setSpeed (right, wl);
+  }
+  vel.linear.x=wr;
+  vel.linear.y=wl;
 }
 
 ros::Subscriber<geometry_msgs::Twist> sub("/cmd_vel", &motors_cb);
+ros::Publisher pub("/info_vel", &vel);
 //ros::Publisher pub("/odom", &mov);
 
 void setup ()
@@ -69,9 +88,10 @@ void setup ()
 
   nh.initNode(); // initialize ROS node
   nh.subscribe(sub);
-  //nh.advertise(pub);
+  nh.advertise(pub);
 }
 
 void loop(){
+  pub.publish(&vel);
   nh.spinOnce();
 }
