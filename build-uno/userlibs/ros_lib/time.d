b6bb24fde39b2b8ddd56470033beb53ac62b0e97
Diego <diego.gibellofoglio@gmail.com>
2018-03-22 09:56:58 +0100

    Adding version 2 of the project

--- build-uno/userlibs/ros_lib/time.d
+++
@@ -1,4 +0,0 @@
-build-uno/userlibs/ros_lib/time.o: \
- /home/diego/arduino_ws/libraries/ros_lib/time.cpp \
- /home/diego/arduino_ws/libraries/ros_lib/ros/time.h \
- /home/diego/arduino_ws/libraries/ros_lib/ros/duration.h
