5a1335674e46b09c06f04d566854fa032ae3c984
Diego <diego.gibellofoglio@gmail.com>
2018-03-26 09:23:29 +0200

    Up code using c++

--- Vs_2/Motor_Cnt2.ino
+++ Vs_2/Motor_Cnt2.ino
@@ -1,64 +1,46 @@
 #include <ros.h>
 #include <StepperDriver.h>
 #include <geometry_msgs/Twist.h>
+#include <std_msgs/Float32.h>
+#include "Motor.hpp"
+
 
 ros::NodeHandle  nh; // allows to create publisher/subscriber
 geometry_msgs::Twist vel;
+std_msgs::Float32 msg;
+
+Motor right_mt(2, 5);
+Motor left_mt(3, 6);
 
 #define EN 8
-#define L 0.20 // distance between the two wheels of the robot
-#define R 0.05 //radius of the wheel od the robot
+#define L 0.20f // distance between the two wheels of the robot
+#define R 0.05f //radius of the wheel od the robot
 
-const float A = L/(2*R);
-const float B = 1/R;
+const float A = L/(2.0f*R);
+const float B = 1.0f/R;
 
-axis_t right_mt, left_mt;
 int cnt;
-
-int sign(float x)
-{
-    if(x > 0) return 1;
-    if(x < 0) return -1;
-    return 0;
-}
-
-void set_motors( axis_t motor, float w )
-{
-  float w_abs;
-  int w_s;
-
-  w_abs = abs(w);
-  w_s = sign(w);
-
-  if(w_s > 0){
-    StepperDriver.setDir (motor, FORWARD);
-    StepperDriver.setSpeed (motor, w_abs);
-  }else{
-    StepperDriver.setDir (motor, BACKWARD);
-    StepperDriver.setSpeed (motor, w_abs);
-  }
-}
-
 
 void motors_cb(const geometry_msgs::Twist &move)
 {
-  float wr,wl;
+  const float w_right = A*move.angular.z + B*move.linear.x;
+  const float w_left = A*move.angular.z - B*move.linear.x;
 
-  wr = A*move.angular.z + B*move.linear.x;
-  wl = A*move.angular.z - B*move.linear.x;
-
-  set_motors(right_mt, wr);
-  set_motors(left_mt, wl);
+  right_mt.set_speed(w_right);
+  left_mt.set_speed(w_left);
 
   cnt ++;
 
   vel.linear.x = move.linear.x;
   vel.angular.z = move.angular.z;
 
+  msg.data = w_right;
+
 }
 
 ros::Subscriber<geometry_msgs::Twist> sub("/cmd_vel", &motors_cb);
 ros::Publisher pub("/info_vel", &vel);
+ros::Publisher pubtest("/test_speed", &msg);
 
 void setup ()
 {
@@ -68,13 +50,8 @@ void setup ()
 
   StepperDriver.init();
 
-  right_mt = StepperDriver.newAxis(2, 5, 255, 200);
-  left_mt = StepperDriver.newAxis(3, 6, 255, 200);
-
-  StepperDriver.enable(right_mt);
-  StepperDriver.enable(left_mt);
-
-  nh.initNode(); // initialize ROS node
+  nh.initNode(); // initialize ROS nodes
+  nh.advertise(pubtest);
   nh.subscribe(sub);
   nh.advertise(pub);
 }
@@ -84,7 +61,11 @@ void loop()
   if((cnt%10) == 1)
   {
     pub.publish(&vel);
+    pubtest.publish(&msg);
   }
+
+  left_mt.run();
+  right_mt.run();
 
   nh.spinOnce();
 }
